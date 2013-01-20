9db31803a022a0828be9147bc8e62ec47e4e6748
benyami <ben@benjaminolson.net>
2013-01-20 01:52:23 -0700

    Did a lot of stuff. It is late. Zzzzzz.

---
+++ html/js/dontworry.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+
+	 // $.backstretch([
+      // "dontworry-lowres50%.jpg"
+  	// ], {duration: 3000, fade: 750});
+
+    // auto increase text area for plans
+    $('#as').autosize();  
+
+
+});
+
+
--- html/js/typica-login.js
+++
@@ -1,9 +0,0 @@
-// jQuery(document).ready(function($) {
-
-// 	$.backstretch([
-//       "dontworry-lowres50%.jpg"
-//   	], {duration: 3000, fade: 750});
-		
-// });
-
-
