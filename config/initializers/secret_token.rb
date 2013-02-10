2a4f63f5cac8f74e2467e737f723db20bee73e53
benyami <ben@benjaminolson.net>
2013-02-09 17:45:05 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dontworry::Application.config.secret_token = '4c954ac0fc3f1d485140e77d6d647af167f039f087ad38622909f2acc977a7be539b55b4599722eef13caba292580c3af46d83638698945d1ebe59eb5d45584f'
