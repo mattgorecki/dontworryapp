71ef1693f30880561e6cfe2c4e35a5275090505d
benyami <ben@benjaminolson.net>
2013-02-23 14:40:52 -0700

    devise needs this

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,9 @@ module Dontworry
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Devise said this was good for Heroku. Forces application to not access 
+    # the DB or load models when precompiling assets.
+    config.assets.initialize_on_precompile = false
   end
 end
