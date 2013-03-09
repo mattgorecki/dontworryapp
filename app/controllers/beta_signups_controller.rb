7ed3ea0a53af1d9182e7f0242752bd2c811cde53
benyami <ben@benjaminolson.net>
2013-03-09 16:53:45 -0700

    add email for beta signups

--- app/controllers/beta_signups_controller.rb
+++ app/controllers/beta_signups_controller.rb
@@ -8,6 +8,10 @@ class BetaSignupsController < ApplicationController
   def create
     @beta_user = BetaSignup.new(params[:beta_signup])
     if @beta_user.save
+      ## USE WITHOUT SIDEKIQ
+      UserMailer.beta_signup_confirmation(@beta_user).deliver
+      ## USE WITH SIDEKIQ
+      # UserMailer.delay.beta_signup_confirmation(@beta_user)
       flash[:success] = "Added to Beta Announcement List"
       render 'success', layout: 'prelaunch'
     else
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ActionMailer::Base
+  default from: "dontworryapp@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.beta_signup_confirmation.subject
+  #
+  def beta_signup_confirmation(user)
+    @user = user
+
+    mail to: @user.email
+  end
+end
