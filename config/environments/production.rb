d8c403af659b77c6202a0bacdeeb9782e89afab5
benyami <ben@benjaminolson.net>
2013-03-09 20:41:43 -0700

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Dontworry::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( prelaunch/beta_signups.css.scss )
+  config.assets.precompile += %w( prelaunch/beta_signups.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
