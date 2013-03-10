3e2452af02b54cd4114395d47c7c05819bd7a97c
benyami <ben@benjaminolson.net>
2013-03-09 17:08:04 -0700

    WIP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Dontworry::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    # domain: "dontworryapp.com",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
