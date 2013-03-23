0dacee8c96f404198a82f35f816418648a23d2e8
benyami <ben@benjaminolson.net>
2013-03-23 15:59:25 -0600

    respect RedisToGo Nano connection limits

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,25 @@
+require 'sidekiq'
+
+## Our production Redis has limitations. Don't go over the # of connections allowed.
+## Consider the # of web workers
+## Consider the # of web worker thread threads (currently in config/unicorn.rb)
+## Consider the # of worker dynos connection to redis.
+##
+## max connections = (Heroku worker count * (concurrency + 2 reserved connections)) + (web dyno count * (client connection size * unicorn worker_process size))
+##
+## AWESOME TOOL: http://manuel.manuelles.nl/sidekiq-heroku-redis-calc/
+## also http://manuel.manuelles.nl/blog/2012/11/13/sidekiq-on-heroku-with-redistogo-nano/
+##
+## Also change config/sidekiq.yml
+
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+
+Sidekiq.configure_server do |config|
+  # The config.redis is calculated by the 
+  # concurrency value so you do not need to 
+  # specify this. For this demo I do 
+  # show it to understand the numbers
+  config.redis = { :size => 7 }
+end
