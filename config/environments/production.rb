5d825c85e957e9e61807b5885a33e86811515485
benyami <ben@benjaminolson.net>
2013-03-28 22:01:37 -0600

    adding threads to puma server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Dontworry::Application.configure do
   config.action_mailer.default_url_options = {host: "www.dontworryapp.com"}
 
   # Enable threaded mode
-  # config.threadsafe!
+  config.threadsafe!
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
---
+++ config/puma.rb
@@ -0,0 +1,6 @@
+# Configure “min” to be the minimum number of threads to use to answer
+# requests and “max” the maximum.
+#
+# The default is “0, 16”.
+#
+threads 0, 4
