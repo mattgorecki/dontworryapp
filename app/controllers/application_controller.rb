46ffd75365e307493d7f6f657cd8ff788eea6c13
benyami <ben@benjaminolson.net>
2013-03-29 22:22:15 -0600

    enforce basic auth in staging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  if Rails.env.staging? 
+    http_basic_authenticate_with :name => "dont", :password => "worryman" 
+  end
+
 end
