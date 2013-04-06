e637a57f61dbe206d1ef2f066638f0680651d4da
benyami <ben@benjaminolson.net>
2013-04-06 11:58:32 -0600

    using 4 threads in puma. take one away from sidekiq for redis concurrency limit of 10

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -25,7 +25,7 @@ Sidekiq.configure_server do |config|
   # concurrency value so you do not need to 
   # specify this. For this demo I do 
   # show it to understand the numbers
-  config.redis = { :size => 7, :url => ENV["REDISCLOUD_URL"], :namespace => 'dontworry' }
+  config.redis = { :size => 6, :url => ENV["REDISCLOUD_URL"], :namespace => 'dontworry' }
   # if Rails.env.production?
   #   config.redis[:url] = ENV["REDISCLOUD_URL"]
   #   config.redis[:namespace] = 'dontworry'
