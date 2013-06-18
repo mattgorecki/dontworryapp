51f6b73d4b44e680e4c99afdb3edcd5a433f6efa
benyami <ben@benjaminolson.net>
2013-06-17 19:32:30 -0600

    mailer specs

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,17 +1,23 @@
 require "spec_helper"
 
 describe UserMailer do
+  
+  before do
+    @user = User.new(name: "Example User", email: "user@example.org", 
+                   password: "foobar", password_confirmation: "foobar")
+  end
+
   describe "beta_signup_confirmation" do
-    let(:mail) { UserMailer.beta_signup_confirmation }
+    let(:mail) { UserMailer.beta_signup_confirmation(@user) }
 
     it "renders the headers" do
-      mail.subject.should eq("Beta signup confirmation")
-      mail.to.should eq(["to@example.org"])
-      mail.from.should eq(["from@example.com"])
+      mail.subject.should eq("Thanks from Don't Worry!")
+      mail.to.should eq(["user@example.org"])
+      mail.from.should eq(["dontworryapp@gmail.com"])
     end
 
     it "renders the body" do
-      mail.body.encoded.should match("Hi")
+      mail.body.encoded.should match("Thank you for signing up for Don't Worry!\r\n\r\nWe are hard at work on the Don't Worry app and we will be sending you emails about our progress.\r\n\r\nYou can really help us out by sharing with your friends. http://www.dontworryapp.com\r\n\r\nThank you,\r\nThe Don't Worry Team")
     end
   end
 
