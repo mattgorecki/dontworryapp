b65ba713bd15635b2fb3b10ed9a51d39046c49b7
benyami <ben@benjaminolson.net>
2013-06-18 10:23:59 -0600

    status worker WIP

--- app/workers/check_status_worker.rb
+++ app/workers/check_status_worker.rb
@@ -4,37 +4,61 @@
 class CheckStatusWorker
   include Sidekiq::Worker
 
+  # core method called by Sidekiq
   def perform(adventure_id)
-    # Let the other workers know we are working on this.
-    @locking_key = "adventure_wip_#{adventure_id}"
+
+    @current_worker_timestamp = Time.now.utc.to_i
+    @locking_key = "adventure_modify_#{adventure_id}"
 
     if unlocked?
-      # Fetch evidence
+      
       @adventure = Adventure.where(id: adventure_id).first
-
+      
+      mark_worker_was_here
       determine_adventure_state
       take_action_based_on_state
+      record_actions
 
       remove_lock
+
     else
-      # should we go away or reschedule?
+
       puts "adventure #{adventure_id} was locked"
+      # something is stuck
+      if (@current_worker_timestamp - @active_worker_timestamp) > 120
+        # TODO: Log that bad is happening.
+        # TODO: Clear stuck worker?
+        # TODO: Restart this stuff.
+      end
+
     end
   end
 
 
+  
+
+  # Check to see if any other worker is working on this document.
+  # unlocked => true/false if we are first to set @locking_key
+  # @active_worker_timestamp => active worker's timestamp / same as current if unlocked
   def unlocked?
-    unlocked, _ = Sidekiq.redis do |redis| 
+    unlocked, _, @active_worker_timestamp = Sidekiq.redis do |redis| 
       redis.multi do
-        redis.setnx(@locking_key, 'wip')
-        redis.expire(@locking_key, 60)
+        redis.setnx(@locking_key, @current_worker_timestamp)
+        redis.expire(@locking_key, 600)
+        redis.get(@locking_key)
       end
     end
     unlocked
   end
 
+  # Remove the redis key associated with this worker working.
   def remove_lock
     Sidekiq.redis { |redis| redis.del(@locking_key) }
+  end
+
+  # Mark evidence that this worker ran.
+  def mark_worker_was_here
+    # @adventure.event.create(subject: 'worker', verb: 'run')
   end
 
   def determine_adventure_state
@@ -48,6 +72,10 @@ class CheckStatusWorker
     if @adventure_state == 'not implented'
       puts 'nothing to see here'
     end
+  end
+
+  def record_actions
+
   end
 
 end
