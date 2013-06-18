3f1d750454b3d558740dcd49c8cde6e46225b779
benyami <ben@benjaminolson.net>
2013-06-17 20:10:02 -0600

    basic loop for checking adventure events

--- app/workers/check_status_worker.rb
+++ app/workers/check_status_worker.rb
@@ -1,10 +1,15 @@
+# run manually: load "#{Rails.root}/app/workers/check_status_worker.rb"
+
 class CheckStatusWorker
   include Sidekiq::Worker
   def perform(adventure_id)
     # Fetch evidence
+    adventure = Adventure.where(id: adventure_id).first
 
     # Check state
-
+    adventure.events.each do |event|
+      puts event.inspect
+    end
     # Take action
   end
 end
