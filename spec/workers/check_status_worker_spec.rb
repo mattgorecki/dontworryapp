7f1a2f7387fa38cffeb596d7620dd090504cdca6
benyami <ben@benjaminolson.net>
2013-06-20 15:12:19 -0600

    worker specs

---
+++ spec/workers/check_status_worker_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'sidekiq/testing'
+
+describe CheckStatusWorker do
+
+  status_worker = CheckStatusWorker.new
+
+  describe "perform_async" do
+    it 'should increment the job count' do
+      expect {
+        CheckStatusWorker.perform_async(FactoryGirl.create(:adventure))
+      }.to change(CheckStatusWorker.jobs, :size).by(1)
+    end
+  end
+
+  describe "perform_at" do
+    it 'should schedule a job' do
+      adventure = FactoryGirl.create(:adventure)
+      CheckStatusWorker.perform_at(Time.new(2014,1,23,14,00), adventure.id)
+      CheckStatusWorker.should have_queued_job_at(Time.new(2014,1,23,14,00),adventure.id)
+    end
+  end
+
+  describe "#perform" do
+    it 'should return successfully' do
+      adventure = FactoryGirl.create(:adventure)
+      status_worker.perform(adventure.id).should eq 1
+    end
+  end
+end
