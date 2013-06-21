97e23fcbebf8d68ccd89bae6d9c401253e0348df
benyami <ben@benjaminolson.net>
2013-06-21 14:25:31 -0600

    adventure should have some required events

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -8,4 +8,26 @@ class Adventure
 
   embeds_many :events, class_name: "HistoryEvent"
   belongs_to :user
+
+  after_create :add_event_adventure_created
+
+
+  def add_event_adventure_created
+    events.create(action: 'adventure_created')
+  end
+
+  def validate_base_events_exist
+    base_events = %w{
+      adventure_created
+      time_start_set
+      time_end_set
+      time_alert_set
+      worker_scheduled
+    }
+    events.each do |event|
+      return false unless event.valid?
+      base_events.delete(event.action)
+    end
+    base_events.count == 0
+  end
 end
--- spec/models/adventure_spec.rb
+++ spec/models/adventure_spec.rb
@@ -1,15 +1,65 @@
 require 'spec_helper'
 
 describe Adventure do
-  user = {}
-  user[:id] = 1
+
+  let(:adventure) { FactoryGirl.create(:adventure) }
 
   it { should_not allow_mass_assignment_of(:user_id) }
+  
 
   describe "#new" do
     it "should create a new one" do
-      FactoryGirl.create(:adventure).should be_valid
+      expect(adventure).to be_valid
+    end
+  end
 
+  describe "embeded events" do
+    it { should respond_to(:events) }
+
+    it "should automatically create adventure_created event" do
+      expect(adventure.events.first.action).to eq('adventure_created')
+    end
+
+    describe "#validate_base_events_exist" do
+      it "should return false if all base_events are not present" do
+        expect(adventure.validate_base_events_exist).to eq(false)
+      end
+
+      it "should return true if all base_events are present" do
+        adventure.events << ScheduleEvent.new(action: 'time_start_set', time: Time.now)
+        adventure.events << ScheduleEvent.new(action: 'time_end_set', time: Time.now + 1.minute)
+        adventure.events << ScheduleEvent.new(action: 'time_alert_set', time: Time.now + 2.minutes)
+        adventure.events << ScheduleEvent.new(action: 'worker_scheduled', time: Time.now + 1.minute)
+        expect(adventure.validate_base_events_exist).to eq(true)
+      end
+
+      it "should return false if time_start_set is missing" do
+        adventure.events << ScheduleEvent.new(action: 'time_end_set', time: Time.now + 1.minute)
+        adventure.events << ScheduleEvent.new(action: 'time_alert_set', time: Time.now + 2.minutes)
+        adventure.events << ScheduleEvent.new(action: 'worker_scheduled', time: Time.now + 1.minute)
+        expect(adventure.validate_base_events_exist).to eq(false)
+      end
+
+      it "should return false if time_end_set is missing" do
+        adventure.events << ScheduleEvent.new(action: 'time_start_set', time: Time.now)
+        adventure.events << ScheduleEvent.new(action: 'time_alert_set', time: Time.now + 2.minutes)
+        adventure.events << ScheduleEvent.new(action: 'worker_scheduled', time: Time.now + 1.minute)
+        expect(adventure.validate_base_events_exist).to eq(false)
+      end
+
+      it "should return false if time_alert_set is missing" do
+        adventure.events << ScheduleEvent.new(action: 'time_start_set', time: Time.now)
+        adventure.events << ScheduleEvent.new(action: 'time_end_set', time: Time.now + 1.minute)
+        adventure.events << ScheduleEvent.new(action: 'worker_scheduled', time: Time.now + 1.minute)
+        expect(adventure.validate_base_events_exist).to eq(false)
+      end
+
+      it "should return false if worker_scheduled is missing" do
+        adventure.events << ScheduleEvent.new(action: 'time_start_set', time: Time.now)
+        adventure.events << ScheduleEvent.new(action: 'time_end_set', time: Time.now + 1.minute)
+        adventure.events << ScheduleEvent.new(action: 'time_alert_set', time: Time.now + 2.minutes)
+        expect(adventure.validate_base_events_exist).to eq(false)
+      end
     end
   end
 end
